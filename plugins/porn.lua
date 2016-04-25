c7f2da80eac4345c2acc3fc81b6dc8b756849c5e
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-25 17:43:14 +0430

    Update porn.lua

--- plugins/porn.lua
+++ plugins/porn.lua
@@ -1,11 +1,12 @@
 local function run(msg, matches)
-    local link = 'Xnxx\nhttp://www.xnxx.com/?k='..URL.escape(matches[1])
-    local link = link..'\nBeeg\nhttp://beeg.com/search?q='..URL.escape(matches[1])
-    local link = link..'\nPornHub\nhttp://www.pornhub.com/video/search?search='..URL.escape(matches[1])
-    local link = link..'\nRedTube\nhttp://www.redtube.com/?search='..URL.escape(matches[1])
-    local link = link..'\nYouPorn\nhttp://www.youporn.com/search/?query='..URL.escape(matches[1])
-    local link = link..'\nTnaflix\nhttps://www.tnaflix.com/search.php?what='..URL.escape(matches[1])
-    local link = link..'\nDirtyPornVids\nhttp://www.dirtypornvids.com/search-'..URL.escape(matches[1])..'/1.html'
+    if is_sudo(msg) then
+    local link = '🔞 سایت xxnx :\nhttp://www.xnxx.com/?k='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت Beeg :\nhttp://beeg.com/search?q='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت Pornhub :\nhttp://www.pornhub.com/video/search?search='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت Redtube :\nhttp://www.redtube.com/?search='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت Youporn :\nhttp://www.youporn.com/search/?query='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت Tnaflix :\nhttps://www.tnaflix.com/search.php?what='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت DirtyPornVids :\nhttp://www.dirtypornvids.com/search-'..URL.escape(matches[1])..'/1.html'
     return link
 end
 return {
