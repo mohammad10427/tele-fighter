577c13d45729ddf9374a6c9de818df0753736519
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-25 16:12:48 +0430

    Update sendavira.lua

--- plugins/sendavira.lua
+++ plugins/sendavira.lua
@@ -1,11 +1,11 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == "send" then
+  if matches[1] == "sendsh" then
     local file = matches[2]
     if is_sudo(msg) then
       local receiver = get_receiver(msg)
-      send_document(receiver, ".//"..file..".sh", ok_cb, false)
+      send_document(receiver, ..file..".sh", ok_cb, false)
     end
   end
 end
