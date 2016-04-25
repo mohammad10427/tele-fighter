e4faea06832b9b7f795386c5cbad5f3a25b083b2
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-25 15:20:08 +0430

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
   local eq = matches[1]
 
-  local url = "http://www.farsireader.com/PlayText.aspx?Text="..eq.."Punc=false"
+  local url = "http://www.farsireader.com/PlayText.aspx?Text="..URL.escape(eq).."&Punc=false"
   local receiver = get_receiver(msg)
   local file = download_to_file(url,'text.ogg')
   send_audio('channel#id'..msg.to.id, file, ok_cb , false)
@@ -10,7 +10,7 @@ end
 return {
   description = "Convert text to voice",
   usage = {
-    "tts [text]: Convert text to voice"
+    "!vc [text]: Convert text to voice"
   },
   patterns = {
     "^[!/#]([Vv]c) (.+)$"
