282de4a969606f3b156ad046f6f8fbbf4a712da4
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-25 17:49:27 +0430

    Update porn.lua

--- plugins/porn.lua
+++ plugins/porn.lua
@@ -1,5 +1,4 @@
 local function run(msg, matches)
-    is_sudo(msg)
     local link = '🔞 سایت xxnx :\nhttp://www.xnxx.com/?k='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت Beeg :\nhttp://beeg.com/search?q='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت Pornhub :\nhttp://www.pornhub.com/video/search?search='..URL.escape(matches[1])
@@ -7,6 +6,8 @@ local function run(msg, matches)
     local link = link..'\n\n🔞 سایت Youporn :\nhttp://www.youporn.com/search/?query='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت Tnaflix :\nhttps://www.tnaflix.com/search.php?what='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت DirtyPornVids :\nhttp://www.dirtypornvids.com/search-'..URL.escape(matches[1])..'/1.html'
+    
+    if is_sudo(msg) then
     return link
 end
 return {
