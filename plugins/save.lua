398ff5467fb9a39b1469e0c2e768baa8e3d5b9ce
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-26 14:48:49 +0430

    Create save.lua

---
+++ plugins/save.lua
@@ -0,0 +1,29 @@
+local function saveplug(msg, success, result)
+  local receiver = get_receiver(msg)
+  if success then
+    local file = './plugins/'..name..'.lua'
+    print('File saving to:', result)
+    os.rename(result, file)
+    print('File moved to:', file)
+  else
+    print('Error downloading: '..msg.id)
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
+  end
+end
+local function run(msg,matches)
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
+    if msg.reply_id then
+   local name = matches[2]
+      if matches[1] == "save" and matches[2] then
+load_document(msg.reply_id, saveplug, msg)
+        return 'در لیست پلایگن ها ذخیره شد '..name
+    end
+end
+end
+return {
+  patterns = {
+ "^[!/#](save) (.*)$",
+  },
+  run = run,
+}
