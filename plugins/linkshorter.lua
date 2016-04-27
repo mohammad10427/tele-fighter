134fac7c11b80e899212957f2b52ae361f2cdfd1
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 17:04:05 +0430

    Update linkshorter.lua

--- plugins/linkshorter.lua
+++ plugins/linkshorter.lua
@@ -1,3 +1,13 @@
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
+
 local function run(msg, matches)
   local Mohammad = URL.escape(matches[1])
   url = "https://api-ssl.bitly.com/v3/shorten?access_token=f2d0b4eabb524aaaf22fbc51ca620ae0fa16753d&longUrl="..Mohammad
@@ -6,7 +16,7 @@ local function run(msg, matches)
   if jdat.message then
     return '🌀 لینک کوتاه شده : \n___________\n\n'..jdat.message
   else
-    return "🌐 لینک اصلی : \n_____________________________\n"..jdat.data.long_url.."\n\nلینک کوتاه شده:\n_____________________________\n"..jdat.data.url
+    return "🌐 لینک اصلی : "..jdat.data.long_url.."\n\n✂️ لینک کوتاه شده : "..jdat.data.url
     end
   end
 
@@ -16,3 +26,13 @@ return {
   },
   run = run,
 }
+
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
