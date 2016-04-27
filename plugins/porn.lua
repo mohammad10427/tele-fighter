e4df1e38fc8f253c3c77a05d5d70168f9771d653
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 17:26:39 +0430

    Update porn.lua

--- plugins/porn.lua
+++ plugins/porn.lua
@@ -1,3 +1,13 @@
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
+
 local function run(msg, matches) 
     local mohammad = 179071599
         if msg.from.id == tonumber(mohammad) then
@@ -7,6 +17,7 @@ local function run(msg, matches)
     local link = link..'\n\n🔞 سایت Redtube :\nhttp://www.redtube.com/?search='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت Youporn :\nhttp://www.youporn.com/search/?query='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت Tnaflix :\nhttps://www.tnaflix.com/search.php?what='..URL.escape(matches[1])
+    local link = link..'\n\n🔞 سایت YouTube :\nhttps://www.youtube.com/results?search_query='..URL.escape(matches[1])
     local link = link..'\n\n🔞 سایت DirtyPornVids :\nhttp://www.dirtypornvids.com/search-'..URL.escape(matches[1])..'/1.html'
     return link
 end
@@ -17,3 +28,13 @@ return {
     },
 run = run
 }
+
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
