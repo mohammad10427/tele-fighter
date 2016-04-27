fe8b5396a5a34a6a2b39ac1ecdbf40e1ea38e0ff
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 17:30:58 +0430

    Update setabout.lua

--- plugins/setabout.lua
+++ plugins/setabout.lua
@@ -1,3 +1,13 @@
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
+
 local function run(msg, matches,success)
 
 local about = matches[2]
@@ -5,7 +15,7 @@ local chat = 'channel#id'..msg.to.id
         if msg.to.type == 'channel' and matches[1] == 'setabout' then
         local about = matches[2]
         channel_set_about(chat, about, ok_cb, false)
-        return 'درباره گروه تنظیم شد'
+        return 'درباره گروه تنظیم شد به : \n'..about..''
     end
 end
 
@@ -15,3 +25,13 @@ end
     }, 
     run = run 
   }
+
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
