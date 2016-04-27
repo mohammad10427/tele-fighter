ba79701e3b0a28793a0a5e64c3aa80cc0e4d05c6
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 22:12:27 +0430

    Update filtering.lua

--- plugins/filtering.lua
+++ plugins/filtering.lua
@@ -9,10 +9,9 @@
 -- //        \\  \/      || ||        \\  //        \\
 
 local function addword(msg, name)
-    local name = matches[1]
     local hash = 'chat:'..msg.to.id..':badword'
     redis:hset(hash, name, 'newword')
-    return "کلمه '..name..' درون فیلترینگ گروه ثبت شد"
+    return "کلمه درون فیلترینگ گروه ثبت شد"
 end
 
 local function get_variables_hash(msg)
