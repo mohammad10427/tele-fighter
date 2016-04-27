28f774aba44cc0f6215dee79e851c554751c27bf
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 16:40:15 +0430

    Update dad.lua

--- plugins/dad.lua
+++ plugins/dad.lua
@@ -1,3 +1,13 @@
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
+
 do
 
 function run(msg, matches)
@@ -9,6 +19,39 @@ reply_msg(reply_id, text, ok_cb, false)
 end
 end 
 end
+
+do
+function run(msg, mathches)
+local relpy_id = msg['id']
+local text = 'سلام مدیر اصلی گروه 😘'
+if mathches[1] == 'سلام' then
+    if is_owner(msg) then
+        reply_msg(reply_id, text, ok_cb, false)
+    end
+end
+end
+
+do
+function run(msg, mathches)
+local relpy_id = msg['id']
+local text = 'سلام معاون گروه 😘'
+if mathches[1] == 'سلام' then
+    if is_momod(msg) then
+        reply_msg(reply_id, text, ok_cb, false)
+    end
+end
+end
+
+do
+function run(msg, mathches)
+local relpy_id = msg['id']
+local text = 'سلام'
+if mathches[1] == 'سلام' then
+    if is_member(msg) then
+        reply_msg(reply_id, text, ok_cb, false)
+    end
+end
+end
 return {
 patterns = {
     "^سلام$",
@@ -17,3 +60,13 @@ run = run
 }
 
 end
+
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
