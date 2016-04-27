42518d2f4d33d1d5839f7a6294bfad400149a2a5
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-27 17:01:22 +0430

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -1,13 +1,23 @@
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
+
 do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "مجاز برای مدیر و ادمین ها"
+        return "فقط مخصوص مدیران می باشد"
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "اول باید لینک جدید ایجاد کنید"
+        return "اول باید لینک جدید ایجاد کنید\nسپس شماره ربات را سیو کنید\nتوجه کنید که باید ربات سازنده گروه باشد در غیر این صورت لینک به پیوی شما ارسال نمی شود"
        end
          local text = "لینک گروه:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
@@ -22,3 +32,13 @@ return {
 }
 
 end
+
+-- By AviraTeam https://telegram.me/avirateam
+-- Devloper : MohammadArak https://telegram.me/mohammadarak
+-- Bot ID: @avirabot
+--      /\  \\        // ||  //------\\        /\
+--     //\\  \\      //  || ||       ||       //\\
+--    //__\\  \\    //   || ||______//       //__\\
+--   //____\\  \\  //    || ||      \\      //____\\
+--  //      \\  \\//     || ||       \\    //      \\
+-- //        \\  \/      || ||        \\  //        \\
