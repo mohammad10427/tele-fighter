27adc0839197ae4f81d6b9fff99d2e9a26237748
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-28 10:01:57 +0430

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+read -p "Do you want me to launch rebooter? (yes/no):"
+
+if [ "$REPLY" != "yes" ]; then
+	echo "
+"
+else 
+        echo "Rebooter"
+bld=$'\e[1m'
+rst=$'\e[0m'
+
+cat << EOF
+
+ $f1  ▀▄   ▄▀     $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4  ▀▄   ▄▀     $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
+ $f1 ▄█▀███▀█▄    $f2███▀▀██▀▀███   $f3▄█▀██▀█▄   $f4 ▄█▀███▀█▄    $f5███▀▀██▀▀███   $f6▄█▀██▀█▄$rst
+ $f1█▀███████▀█   $f2▀▀███▀▀███▀▀   $f3▀█▀██▀█▀   $f4█▀███████▀█   $f5▀▀███▀▀███▀▀   $f6▀█▀██▀█▀$rst
+ $f1▀ ▀▄▄ ▄▄▀ ▀   $f2 ▀█▄ ▀▀ ▄█▀    $f3▀▄    ▄▀   $f4▀ ▀▄▄ ▄▄▀ ▀   $f5 ▀█▄ ▀▀ ▄█▀    $f6▀▄    ▄▀$rst
+ 
+EOF
+echo -e "                \e[100m                Steady script           \e[00;37;40m"
+echo -e "               \e[01;34m                    by iicc                \e[00;37;40m"
+echo ""
+cat << EOF
+ $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
+ $bld$f1█▄█▀███▀█▄█   $f2███▀▀██▀▀███   $f3▄█▀██▀█▄   $f4█▄█▀███▀█▄█   $f5███▀▀██▀▀███   $f6▄█▀██▀█▄$rst
+ $bld$f1▀█████████▀   $f2▀▀▀██▀▀██▀▀▀   $f3▀▀█▀▀█▀▀   $f4▀█████████▀   $f5▀▀▀██▀▀██▀▀▀   $f6▀▀█▀▀█▀▀$rst
+ $bld$f1 ▄▀     ▀▄    $f2▄▄▀▀ ▀▀ ▀▀▄▄   $f3▄▀▄▀▀▄▀▄   $f4 ▄▀     ▀▄    $f5▄▄▀▀ ▀▀ ▀▀▄▄   $f6▄▀▄▀▀▄▀▄$rst
+
+EOF
+
+sudo luarocks install 30log
+
+sudo luarocks install abelhas
+
+sudo luarocks install serpent
+
+sudo luarocks install feedparser
+
+sudo luarocks install lua-cjson
+
+sudo luarocks install luasec
+
+sudo luarocks install luasocket
+
+sudo luarocks install luafilesystem
+
+sudo luarocks install luacrypto
+
+sudo luarocks install luaexpat
+
+sudo luarocks install lub
+
+
+fi
