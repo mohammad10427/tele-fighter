f2bf9751008fbacfe9bb09ed833ba58455efada2
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-04-28 18:11:25 +0430

    Update remmsg.lua

--- plugins/remmsg.lua
+++ plugins/remmsg.lua
@@ -19,7 +19,7 @@ send_msg(extra.chatid, 'تمام پیام های سوپر گروه حذف شد',
 end
 end
 local function run(msg, matches)
-if matches[1] == 'msgrem' and is_owner(msg) then
+if matches[1] == 'msgrem' and is_momod(msg) then
             if msg.to.type == 'channel' then
             if tonumber(matches[2]) > 99 or tonumber(matches[2]) < 1 then
             return "خطا عدد انتخاب شده باید زیر 100 باشد"
