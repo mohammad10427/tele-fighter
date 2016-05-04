e1d1e95702d1bfde7e5b069cfb373e106821d6bd
Mohammad Mahdi <mohammad500ebrahimi@gmail.com>
2016-05-04 16:04:21 +0430

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,3 +1,10 @@
+read -p "Do You Want Me To Run Avira Auto Bot Killer ? (Y/N):"
+
+if [ "$REPLY" != "Y" ]; then
+	echo "
+"
+else 
+
 while true; do
     echo "Killed Avira"
     kill -9 $(pgrep telegram-cli)
